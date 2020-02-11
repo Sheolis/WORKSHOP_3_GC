599cd9b1412902c6998f9374810121bfaa742eb5
Joseph <59622118+Sheolis@users.noreply.github.com>
2020-02-11 11:57:28 +0100

    no bug yet mais je n'ai que un c et un h en visuel

--- dialogue_00/script.js
+++ dialogue_00/script.js
@@ -1,4 +1,5 @@
-var pseudo_liste=['Lui', 'Elle', 'Ça';
+var pseudo_liste=['Lui', 'Elle', 'Ça'];
+var perso_asset_liste=[['docteur_2_0.png',1]]; //Contient le nom d'asset des perso et le slot d'affichage à l'écran de l'image
 var dialogue_index=0; //index du dialogue
 var dialogue_ligne=0; //index de la ligne de texte du dialogue
 var dialogue_liste=[ // contient la liste des dialogues [le dialogue1[ligne de dialogue, l'index du nom(pseudo_list) de celui qui parle], le dialogue2 ...]
@@ -18,48 +19,64 @@ var dialogue_liste=[ // contient la liste des dialogues [le dialogue1[ligne de d
 
 var lien_dialogue_choix= [ //contient à l'index x, l'index du choix appelé(choix_liste) par le dialogue x(dialogue_liste)
   0,
-  0
+  1
 ];
 
-var choix_index = 0;
-var choix_liste [ //contient plusieurs groupes de choix. Chaque choix est composé d'une phrase et de l'index du dialogue qu'il appelle.
+var choix_index;
+var choix_liste = [ //contient plusieurs groupes de choix. Chaque choix est composé d'une phrase et de l'index du dialogue qu'il appelle.
   [
     ['oui',1],
     ['non',1],
     ['peut-etre',0]
+  ],
+  [
+    ['da',0],
+    ['nee',1],
+    ['poporotski',0]
   ]
-]
+];
 
-$('#dialogue').html(dialogue_list[dialogue_index][dialogue_ligne][0]); //appelle la première ligne du premier dialogue à s'afficher sur le html
+var dialogue_statut=1; // 1 le dialogue est en cours, 0 le dialogue est terminé
+$('#dialogue').html(dialogue_liste[dialogue_index][dialogue_ligne][0]); //appelle la première ligne du premier dialogue à s'afficher sur le html
 
 
 $('#dialogue').on('click',function(){
-  if(dialogue_i<dialogue_list.length-1) {
+  if(dialogue_index<dialogue_liste.length-1) {
       dialogue_index++;
       if(dialogue_liste[dialogue_index]=='choix') {
           choix_i=f_choix(choix_index);
       }
-      $('#dialogue').html(dialogue_list[dialogue_index]);
+      $('#dialogue').html(dialogue_liste[dialogue_index]);
   }
 })
 
 function f_dialogue(dialogue_index) {
     dialogue_ligne=0 ;
     while (dialogue_ligne<dialogue_liste[dialogue_index].length){
-        if (dialogue_list[dialogue_index][dialogue_ligne][0]=='choix'){
-            $('#dialogue').html(dialogue_list[dialogue_index][dialogue_ligne][0]);
+        if (dialogue_liste[dialogue_index][dialogue_ligne][0]=='choix'){
+            return f_choix(lien_dialogue_choix[dialogue_index]);
         }
-        else if(dialogue_list[dialogue_index][dialogue_ligne][0]=='fin') {
-            f_choix(choix_liste[lien_dialogue_choix][1])
+        else if(dialogue_liste[dialogue_index][dialogue_ligne][0]=='fin') {
+          return 0;
         }
         else {
-            $('#nom_du_locuteur').html(dialogue_list[dialogue_index][dialogue_ligne][1]);
-            $('#dialogue').html(dialogue_list[dialogue_index][dialogue_ligne][0]);
-
+            $('#nom_du_locuteur').html(dialogue_liste[dialogue_index][dialogue_ligne][1]);
+            $('#dialogue').html(dialogue_liste[dialogue_index][dialogue_ligne][0]);
+            dialogue_ligne ++;
         }
     }
 }
 
 function f_choix(choix_index) { //fonction_choix
-
+    for (var i = 0; i < choix_liste[choix_index].length; i++) {
+        $( "#boite_choix_multiples" ).append( "<p id=\"choix_"+i+"\" class=\"choix\">"+choix_liste[i][0]+"</p>" ); // !! code valable pour un nombre de choix inférieur ou égal à 10
+    }
+    return 1;
 }
+
+$('.choix').on('click', function() {
+    $( ".choix" ).remove();
+    f_dialogue(choix_liste[this.id[6]][1]);
+})
+
+dialogue_statut = f_dialogue(0)
