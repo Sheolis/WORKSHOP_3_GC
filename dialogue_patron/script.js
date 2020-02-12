2b57fdbaee379d306d232784596b747765f3db63
1D6 <joseph.ronzier@gmail.com>
2020-02-12 23:28:25 +0100

    transmission d'un dialogue à un autre implementation de etat_jeu code adapté à la nouvelle classification des assets

--- dialogue_patron/script.js
+++ dialogue_patron/script.js
@@ -1,20 +1,34 @@
-var etat_jeu=2;
-
-var pseudo_liste=['','Mère'];
-var decors_liste=['naissance_','chambre_']; //placé dans le chemin suivant ../_graph/img/perso/
+var pseudo_liste=['Lui', 'Elle', 'Ça'];
 var perso_asset_liste=[
+  ['Docteur_2_0.png',1],
+  ['Docteur_2_0.png',0],
+  ['Docteur_2_0.png',2]
 ]; //Contient le nom d'asset des perso et le slot d'affichage à l'écran de l'image
 var dialogue_liste=[ // contient la liste des dialogues [le dialogue1[ligne de dialogue, l'index du nom(pseudo_list) de celui qui parle], le dialogue2 ...]
   [
-    ['Bienvenue dans la famille NOMDUJOUEUR![NDJ]', 1],
-    ['decors', 1]
+    ['Bonjour', 0],
+    ['Textecourt',1],
+    ['TexteLong TexteLongTexteLong TexteLongTexteLongText LongTexteLongTexteLong TexteLongTexteLongTexteLongTexteLongTexteLong TexteLongTexteLong exteLongTexteLong',2],
+    ['TextemoinslongTextemoinslongTextemoinslongTextemoinslongTextemoinslong',0],
+    ['choix',0] // si le dialogue est égal à 'choix' on va aller chercher l'affichage du choix suivant dans la liste de choix, permettant au joueur de prendre une décision. Le 0 n'a pas d'importance
   ],
   [
-    ['Vous vous trouvez dans ce qui semble être une chambre d’enfant. Un certain nombre de jeux se trouvent à votre portée. Envahi par un sentiment d’excitation, vous vous avancez vers ces derniers afin d’y jouer.', 0],
-    ['fin', 0]
+    ['Début du texte 2',1],
+    ['Fin du texte 2',0],
+    ['choix',1]
   ]
 ];
 var choix_liste = [ //contient plusieurs groupes de choix. Chaque choix est composé d'une phrase et de l'index du dialogue qu'il appelle.
+  [
+    ['oui',1],
+    ['non',1],
+    ['peut-etre',0]
+  ],
+  [
+    ['da',0],
+    ['nee',1],
+    ['poporotski',0]
+  ]
 ];
 
 var dialogue_statut=1; // 1 le dialogue est en cours, 0 le dialogue est terminé
@@ -36,22 +50,15 @@ function print_ligne_dialogue(dialogue_index,dialogue_ligne) {
             window.clearInterval(intervalId);
     }, 100);
   }
-function upload_environnement(index_environnement) {
-  $("#environnement").empty();
-  $("#environnement").append('<img id="env_001" class="" src="../_graph/img/decors/'+decors_liste[index_environnement]+etat_jeu+'.png" alt="fond violet."/>');
-}
 
 function clean_emplacements_perso(){
     for (i=0; i<3; i++) $('#emplacement_'+i).empty();
     }
 
   function print_personnage(i, l) { //fonction chargée de l'update de l'image à charger et du nom à afficher
-    var index_perso = dialogue_liste[i][l][1];
-    $('#nom_du_locuteur').html(pseudo_liste[index_perso]);
-    if (perso_asset_liste.length>0) {
       clean_emplacements_perso()
-      $('#emplacement_'+perso_asset_liste[index_perso][1]).append('<img src="../_graph/img/perso/'+perso_asset_liste[index_perso][0]+etat_jeu+'_0'+'.png" />');
-    }
+      $('#emplacement_'+perso_asset_liste[dialogue_liste[i][l][1]][1]).append('<img src="img/'+perso_asset_liste[dialogue_liste[i][l][1]][0]+'" />');
+      $('#nom_du_locuteur').html(pseudo_liste[dialogue_liste[i][l][1]]);
   }
 
 function f_choix(choix_index) { //fonction_choix
@@ -92,31 +99,23 @@ function f_choix(choix_index) { //fonction_choix
 
 
 //clearInterval();
-upload_environnement(0); //charge le premier décors de la liste
 print_personnage(dialogue_index,0);
 print_ligne_dialogue(dialogue_index,0);//appelle la première ligne du premier dialogue à s'afficher sur le html
 
 $('#boite_de_dialogue').on('click',function(){
-    if ($('#dialogue').html().length < dialogue_liste[dialogue_index][dialogue_ligne-1][0].length) { //permet d'accelerer le dialogue si il n'est pas fini
+    if ($('#dialogue').html().length < dialogue_liste[dialogue_index][dialogue_ligne-1][0].length) {
         window.clearInterval(intervalId);
         $('#dialogue').html('');
         $('#dialogue').html(dialogue_liste[dialogue_index][dialogue_ligne-1][0]);
     }
     else {
+
         if (dialogue_liste[dialogue_index][dialogue_ligne][0]=='choix'){
             choix_index=dialogue_liste[dialogue_index][dialogue_ligne][1];
             f_choix(choix_index);
         }
-        else if(dialogue_liste[dialogue_index][dialogue_ligne][0]=='decors') {
-          upload_environnement(dialogue_liste[dialogue_index][dialogue_ligne][1]);
-          dialogue_index ++;
-          print_personnage(dialogue_index,0);
-          print_ligne_dialogue(dialogue_index,0);
-          dialogue_ligne=1;
-        }
         else if(dialogue_liste[dialogue_index][dialogue_ligne][0]=='fin') {
-          document.location.href = '../dialogue_patron/index.html';
-
+          //! A COMPLETER !// - Condition de fin
         }
         else if(dialogue_ligne<dialogue_liste[dialogue_index].length){
             print_personnage(dialogue_index,dialogue_ligne);
