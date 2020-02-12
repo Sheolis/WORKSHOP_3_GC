bf8567d335e4f11053fcbd9adefe75143a96fe65
antoineogeretpa <antoine-hand@hotmail.com>
2020-02-12 10:42:58 +0100

    .?

--- Tuto1/tuto_js.js
+++ Tuto1/tuto_js.js
@@ -1,15 +1,24 @@
+var objet_deplace;
+
 function allowDrop(ev) {
   ev.preventDefault();
+
 }
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
+  objet_deplace=ev.target.id;
 }
 
+
+
+
+
 function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
-if ('drag2' == 'div6') {
+	ev.preventDefault();
+  	if (ev.target.id==(objet_deplace+'_bloc')) {
+		var data = ev.dataTransfer.getData("text");
+	  	ev.target.appendChild(document.getElementById(data));
+	}
+
 }
