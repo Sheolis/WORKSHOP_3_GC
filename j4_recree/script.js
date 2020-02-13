25290aa9373066564d03c9794dadcbdcba1dc952
antoineogerETPA <antoine-hand@hotmail.com>
2020-02-13 16:36:43 +0100

    Re-timer
    Reprise du timer

--- j4_recree/script.js
+++ j4_recree/script.js
@@ -91,7 +91,7 @@ function create(){
 
 		text = this.add.text(32, 32);
 
-	    timedEvent = this.time.addEvent({ delay: 800, callback: setbomb, callbackScope: this, repeat: 20 });
+	    timedEvent = this.time.addEvent({ delay: 800, callback: setbomb, callbackScope: this, repeat: 25 });
 
 
 
