d949e02d11aac6ae5f5f5ffccd43d1ac088a13bf
antoineogeretpa <antoine-hand@hotmail.com>
2020-02-13 11:45:20 +0100

    h

--- j2_anneau/script.js
+++ j2_anneau/script.js
@@ -18,26 +18,18 @@ scene: {
 };
 
 var game = new Phaser.Game(config);
-var score = 0;
-var vie = 2;
 
 function init() {
 var player;
 var cursors;
-var scoreText;
-var bomb;
 
-var text;
-
-var timedEvent;
 }
 
 function preload(){
-	this.load.image('background','_graph/img/decors/recree_2.png');
-	this.load.image('bomb','_graph/img/assets_jeu/recree_pangBall.png');
-	this.load.image('mur','_graph/img/assets_jeu/recree_mur.png');
-	this.load.image('barriere','_graph/img/assets_jeu/recree_barrieres.png')
-	this.load.spritesheet('perso','_graph/img/assets_jeu/recree_perso2.png',{frameWidth: 64, frameHeight: 64});
+	this.load.image('background','../_graph/img/decors/recree_2.png');
+	this.load.image('perso','../_graph/img/assets_jeu/recree_pangBall.png');
+	this.load.image('mur','../_graph/img/assets_jeu/recree_mur.png');
+	this.load.image('barriere','../_graph/img/assets_jeu/recree_barrieres.png');
 }
 
 
@@ -49,9 +41,6 @@ function create(){
 
 
 	this.add.image(640,360,'background');
-	life1 = this.add.image(400,300,'life1').setScale(0.25);
-	life2 = this.add.image(400,300,'life2').setScale(0.25);
-
 
 	platforms = this.physics.add.staticGroup();
 	platforms.create(0,400,'mur').setScale(2).refreshBody();
@@ -66,7 +55,7 @@ function create(){
 	cursors = this.input.keyboard.createCursorKeys()
 
 	this.physics.add.overlap(player,null,this);
-	this.physics.add.collider(bombs,platforms);
+	this.physics.add.collider(platforms);
 	this.physics.add.collider(player,null, this);
 
 
@@ -91,11 +80,4 @@ function update(){
 
 		player.setVelocityY(0);
 	}
-
-
-
-
-
-
-
-
+}
