1c066d1db9bca6de07c3b47d00c89a5386372018
1D6 <joseph.ronzier@gmail.com>
2020-02-14 06:59:19 +0100

    restart button

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    $('video').on('ended',function(){
+      $("body").fadeOut(500,function(){
+        $('video').addClass('hide');
+      });
+      $("body").fadeIn(500,function(){
+        $('#ecran_start').removeClass('hide');
+      });
+    });
+  });
+
+
+
 $('#start').on('click', function() {
   $.session.set('etat_jeu',1);
   $.session.set('score',0);
